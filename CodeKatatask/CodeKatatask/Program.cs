c45013717f35730132fa09273b1044c0ffaa7107
todreami <140941478+todreami@users.noreply.github.com>
2024-02-27 09:32:41 +0900

    33 incorrect

--- CodeKatatask/CodeKatatask/Program.cs
+++ CodeKatatask/CodeKatatask/Program.cs
@@ -8,7 +8,7 @@ using System.Runtime.Intrinsics;
 //namespace CodeKatatask
 
 
-
+//https://github.com/todreami/SpartaCodeKata.git
 
 
 
@@ -416,6 +416,36 @@ public class Program
 
     //    return answer;
     //}
+
+    //33번 약수의 개수와 덧셈 2.27 오답
+    //public int solution(int left, int right)
+    //{
+    //    int answer = 0;
+    //    int count = 0;
+
+    //    for (int i = left; i <= right; i++)
+    //    {
+    //        for (int j = 1; j <= i; j++)
+    //        {
+    //            if (i % j == 0)
+    //            {
+    //                count++;
+    //            }
+
+    //        }
+
+    //        answer += (count & 1) ? -i : i;
+    //        count = 0;
+    //    }
+
+
+    //    return answer;
+    //}
+
+
+
+
+
 
 
 
