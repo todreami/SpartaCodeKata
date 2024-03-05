1da0aeb12527572c36cdf51102c035b66e909c2d
todreami <140941478+todreami@users.noreply.github.com>
2024-03-05 09:22:32 +0900

    37

--- CodeKatatask/CodeKatatask/Program.cs
+++ CodeKatatask/CodeKatatask/Program.cs
@@ -477,8 +477,20 @@ public class Program
         return answer;
     }
 
+    //37번 문자열 다루기 기본
+    public int[,] solutionthirtyseven(int[,] arr1, int[,] arr2)
+    {
+        int[,] answer = new int[arr1.GetLength(0), arr1.GetLength(1)];
 
-
+        for (int i = 0; i < arr1.GetLength(0); i++)
+        {
+            for (int j = 0; j < arr1.GetLength(1); j++)
+            {
+                answer[i, j] = arr1[i, j] + arr2[i, j];
+            }
+        }
+        return answer;
+    }
 
 
 
