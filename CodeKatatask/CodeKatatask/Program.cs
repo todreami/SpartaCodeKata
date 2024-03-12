17481d85116e53366340bc0b1a9799ab80b5c427
todreami <140941478+todreami@users.noreply.github.com>
2024-03-12 09:57:01 +0900

    42

--- CodeKatatask/CodeKatatask/Program.cs
+++ CodeKatatask/CodeKatatask/Program.cs
@@ -573,6 +573,30 @@ public class Program
         return answer;
     }
 
+    //42번 삼총사
+    public int solutionfortytwo(int[] number)
+    {
+        int cnt = 0;
+        for (int i = 0; i < number.Length; i++)
+        {
+            for (int j = i + 1; j < number.Length; j++)
+            {
+                for (int k = j + 1; k < number.Length; k++)
+                {
+                    if (number[i] + number[j] + number[k] == 0)
+                    {
+                        cnt++;
+                    }
+                }
+            }
+        }
+        return cnt;
+    }
+
+    //43번
+
+
+
 
 }
 
