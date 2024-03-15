6679a2d5e59bbe103035ed57c7276056bae87cdb
todreami <140941478+todreami@users.noreply.github.com>
2024-03-15 09:32:16 +0900

    45
    45

--- CodeKatatask/CodeKatatask/Program.cs
+++ CodeKatatask/CodeKatatask/Program.cs
@@ -622,6 +622,34 @@ public class Program
         return answer;
     }
 
+    //45번 시저 암호
+    public string solutionfortyfive(string s, int n)
+    {
+        string answer = "";
+
+        char[] temp = s.ToCharArray();
+
+        for (int i = 0; i < temp.Length; i++)
+        {
+            if (temp[i] == ' ')
+            {
+                answer += temp[i].ToString();
+                continue;
+            }
+            for (int j = 1; j <= n; j++)
+            {
+                if (temp[i] == 'Z')
+                    temp[i] = 'A';
+                else if (temp[i] == 'z')
+                    temp[i] = 'a';
+                else
+                    temp[i] = (char)(temp[i] + 1);
+            }
+            answer += temp[i].ToString();
+        }
+        return answer;
+    }
+
 
 
 }
