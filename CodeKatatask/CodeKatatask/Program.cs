a9d35aecfd0504436065a71ddf34cd79a8a71f4e
todreami <140941478+todreami@users.noreply.github.com>
2024-03-20 09:21:01 +0900

    48
    48

--- CodeKatatask/CodeKatatask/Program.cs
+++ CodeKatatask/CodeKatatask/Program.cs
@@ -4,6 +4,7 @@ using System.Security.Cryptography;
 using System.Linq;
 using System.Numerics;
 using System.Runtime.Intrinsics;
+using System;
 //using namespace std;
 //namespace CodeKatatask
 
@@ -681,6 +682,36 @@ public class Program
 
         return answer;
     }
+
+    //48번 K번째수
+    public int[] solutionfortyeight(int[] array, int[,] commands)
+    {
+        int[] answer = new int[commands.GetLength(0)];
+
+        for (int index = 0; index < answer.Length; index++)
+        {
+            int i = commands[index, 0];
+            int j = commands[index, 1];
+            int k = commands[index, 2];
+
+            int[] temp = new int[j - i + 1];
+
+            int idx = 0;
+
+            for (int arrayIndex = i - 1; arrayIndex < j; arrayIndex++)
+            {
+                temp[idx++] = array[arrayIndex];
+            }
+
+            Array.Sort(temp);
+
+            answer[index] = temp[k - 1];
+        }
+
+        return answer;
+    }
+
+
 
 
 
