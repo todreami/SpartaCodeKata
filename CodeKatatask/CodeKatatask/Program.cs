f17fe8f6c6041a9634dca8d05e25bba2bd5fcc46
todreami <140941478+todreami@users.noreply.github.com>
2024-03-22 09:24:13 +0900

    50
    50

--- CodeKatatask/CodeKatatask/Program.cs
+++ CodeKatatask/CodeKatatask/Program.cs
@@ -734,6 +734,35 @@ public class Program
         return answers.ToArray();
     }
 
+    public int[] solutionfifty(string s)
+    {
+        int[] answer = new int[s.Length];
+        Dictionary<char, int> values = new Dictionary<char, int>();
+        for (int i = 0; i < s.Length; i++)
+        {
+            char currentChar = s[i];
+            if (values.ContainsKey(currentChar))
+            {
+                int index = values[currentChar];
+                answer[i] = i - index;
+            }
+            else
+            {
+                answer[i] = -1;
+            }
+            values[currentChar] = i;
+        }
+        return answer;
+    }
+
+
+
+
+
+
+
+
+
 
 
 
