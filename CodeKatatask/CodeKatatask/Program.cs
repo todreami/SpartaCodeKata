9226b4bfaffa9300a7d22d33a16556c2f6748e3f
todreami <140941478+todreami@users.noreply.github.com>
2024-03-26 10:06:09 +0900

    52
    52

--- CodeKatatask/CodeKatatask/Program.cs
+++ CodeKatatask/CodeKatatask/Program.cs
@@ -775,8 +775,18 @@ public class Program
         return answer;
     }
 
+    //52번 콜라
+    public int solutionfiftytwo(int a, int b, int n)
+    {
+        int answer = 0;
 
-
+        while (n >= a)
+        {
+            answer += (n / a) * b;
+            n = (n / a) * b + n % a;
+        }
+        return answer;
+    }
 
 
 
