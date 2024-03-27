91ffa44bd0a58637826f6180d42e5782452d502a
todreami <140941478+todreami@users.noreply.github.com>
2024-03-27 09:26:55 +0900

    53
    53

--- CodeKatatask/CodeKatatask/Program.cs
+++ CodeKatatask/CodeKatatask/Program.cs
@@ -788,6 +788,29 @@ public class Program
         return answer;
     }
 
+    //53번 명예의 전당
+    public int[] solutionfiftythree(int k, int[] score)
+    {
+        int[] answer = new int[score.Length];
+        List<int> board = new List<int>();
+
+        for (int i = 0; i < score.Length; i++)
+        {
+            if (board.Count == k)
+            {
+                if (board[0] > score[i])
+                {
+                    answer[i] = board[0];
+                    continue;
+                }
+                board.RemoveAt(0);
+            }
+            board.Add(score[i]);
+            board.Sort();
+            answer[i] = board[0];
+        }
+        return answer;
+    }
 
 
 
