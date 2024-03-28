42944d68ffc8c53e345492cf573187fb3d64e2e1
todreami <140941478+todreami@users.noreply.github.com>
2024-03-28 09:41:44 +0900

    54
    54

--- CodeKatatask/CodeKatatask/Program.cs
+++ CodeKatatask/CodeKatatask/Program.cs
@@ -812,8 +812,26 @@ public class Program
         return answer;
     }
 
+    //54번 2016년
+    public string solutionfiftyfour(int a, int b)
+    {
+        string answer = "";
+        string[] dayName = new string[] { "FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU", };
+        int days = 0;
 
-
+        for (int i = 1; i < a; i++)
+        {
+            if (i == 1 || i == 3 || i == 5 || i == 7 || i == 8 || i == 10 || i == 12)
+                days += 31;
+            else if (i == 2)
+                days += 29;
+            else
+                days += 30;
+        }
+        days += b - 1;
+        answer = dayName[days % 7];
+        return answer;
+    }
 
 
 }
