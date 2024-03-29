36c4338d52c0f6666fb8ed14d90b93761e05009c
todreami <140941478+todreami@users.noreply.github.com>
2024-03-29 09:12:33 +0900

    55
    55

--- CodeKatatask/CodeKatatask/Program.cs
+++ CodeKatatask/CodeKatatask/Program.cs
@@ -833,6 +833,32 @@ public class Program
         return answer;
     }
 
+    //55번 카드뭉치
+    public string solutionfiftyfive(string[] cards1, string[] cards2, string[] goal)
+    {
+        string answer = "Yes";
+
+        int idx1 = 0;
+        int idx2 = 0;
+        for (int i = 0; i < goal.Length; i++)
+        {
+            if (idx1 < cards1.Length && goal[i] == cards1[idx1])
+            {
+                idx1++;
+                continue;
+            }
+            else if (idx2 < cards2.Length && goal[i] == cards2[idx2])
+            {
+                idx2++;
+                continue;
+            }
+            else
+            {
+                answer = "No";
+            }
+        }
+        return answer;
+    }
 
 }
 
