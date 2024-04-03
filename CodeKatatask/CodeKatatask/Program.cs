014572520ebd7d02351b03229ee0e910fa14c599
todreami <140941478+todreami@users.noreply.github.com>
2024-04-03 09:26:27 +0900

    58
    58

--- CodeKatatask/CodeKatatask/Program.cs
+++ CodeKatatask/CodeKatatask/Program.cs
@@ -944,8 +944,33 @@ public class Program
 
 
 
+    //58번 소수 만들기
+    public int solutionfiftyeight(int[] nums)
+    {
+        int answer = 0;
 
 
+        for (int i = 0; i < nums.Length - 2; i++)
+        {
+            for (int j = i + 1; j < nums.Length - 1; j++)
+            {
+                for (int k = j + 1; k < nums.Length; k++)
+                {
+
+                    int n = nums[i] + nums[j] + nums[k];
+
+                    int count = 0;
+                    for (int t = 2; t <= n; t++)
+                    {
+                        if (n % t == 0) count++;
+                    }
+
+                    if (count == 1) answer++;
+                }
+            }
+        }
+        return answer;
+    }
 
 
 
