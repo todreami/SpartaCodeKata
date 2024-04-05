5a816eae382e504a661b057f82e38a8293d37dd0
todreami <140941478+todreami@users.noreply.github.com>
2024-04-05 09:12:04 +0900

    60
    60

--- CodeKatatask/CodeKatatask/Program.cs
+++ CodeKatatask/CodeKatatask/Program.cs
@@ -993,7 +993,29 @@ public class Program
         return answer;
     }
 
+    //60번 기사단원의 무기
+    public int solutionsixty(int number, int limit, int power)
+    {
+        int answer = 0;
+        int[] cnt = new int[number + 1];
+        for (int i = 1; i <= number; i++)
+        {
+            for (int j = i; j <= number; j += i)
+            {
+                cnt[j]++;
+            }
+        }
 
+        for (int i = 1; i <= number; i++)
+        {
+            if (cnt[i] > limit)
+                answer += power;
+            else
+                answer += cnt[i];
+        }
+
+        return answer;
+    }
 
 
 
