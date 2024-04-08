ae8577ed3ef048e3e14b1a0e24b4c3f55d65ab3f
todreami <140941478+todreami@users.noreply.github.com>
2024-04-08 09:40:56 +0900

    61
    61

--- CodeKatatask/CodeKatatask/Program.cs
+++ CodeKatatask/CodeKatatask/Program.cs
@@ -1018,8 +1018,29 @@ public class Program
     }
 
 
-
-
+    //61번 로또의 최고 순위와 최저 순위
+    public int[] solutionsixtyone(int[] lottos, int[] win_nums)
+    {
+        int right = 0;
+        int zeroCount = 0;
+        for (int i = 0; i < lottos.Length; i++)
+        {
+            if (lottos[i] == 0)
+            {
+                zeroCount++;
+            }
+            if (lottos.Contains(win_nums[i]))
+            {
+                right++;
+            }
+        }
+        int MaxCollect = zeroCount + right;
+        int MinCollect = right;
+        if (MinCollect == 0) MinCollect++;
+        if (MaxCollect == 0) MaxCollect++;
+        int[] answer = new int[] { 7 - MaxCollect, 7 - MinCollect };
+        return answer;
+    }
 
 
 
