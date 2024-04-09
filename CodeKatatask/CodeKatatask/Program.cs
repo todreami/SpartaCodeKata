609839108d621631e46bbc6e583fa1c6c93add67
todreami <140941478+todreami@users.noreply.github.com>
2024-04-09 11:11:55 +0900

    62
    62

--- CodeKatatask/CodeKatatask/Program.cs
+++ CodeKatatask/CodeKatatask/Program.cs
@@ -1042,12 +1042,25 @@ public class Program
         return answer;
     }
 
+    //62번 옹알이 (2)
+    public int solutionsixtytwo(string[] babbling)
+    {
+        int answer = 0;
+        string[] talk = new string[] { "aya", "ye", "woo", "ma" };
 
+        for (int i = 0; i < babbling.Length; i++)
+        {
+            for (int j = 0; j < talk.Length; j++)
+            {
+                string str = talk[j] + talk[j];
+                babbling[i] = babbling[i].Replace(str, "@").Replace(talk[j], " ");
+            }
+            if (babbling[i].Trim().Length < 1)
+                answer++;
+        }
 
-
-
-
-
+        return answer;
+    }
 }
 
 
