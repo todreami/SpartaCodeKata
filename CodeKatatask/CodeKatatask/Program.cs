0be04208d836514ff90b1828e665501ce9e1af3b
todreami <140941478+todreami@users.noreply.github.com>
2024-04-11 09:12:04 +0900

    63
    63

--- CodeKatatask/CodeKatatask/Program.cs
+++ CodeKatatask/CodeKatatask/Program.cs
@@ -1061,6 +1061,43 @@ public class Program
 
         return answer;
     }
+
+    //63번 숫자 짝꿍
+    public string solutionsixtythree(string X, string Y)
+    {
+        string answer = "";
+
+        int[] xArray = new int[10];
+        int[] yArray = new int[10];
+
+        for (int i = 0; i < X.Length; i++)
+        {
+            xArray[int.Parse(X[i].ToString())]++;
+        }
+        for (int i = 0; i < Y.Length; i++)
+        {
+            yArray[int.Parse(Y[i].ToString())]++;
+        }
+
+        for (int i = 9; i >= 0; i--)
+        {
+            if (xArray[i] == 0 || yArray[i] == 0)
+                continue;
+
+            int min = Math.Min(xArray[i], yArray[i]);
+
+            answer += string.Concat(Enumerable.Repeat(i, min));
+        }
+
+        if (answer.Length == 0)
+            answer = "-1";
+        else if (answer[0] == '0')
+            answer = "0";
+
+        return answer;
+    }
+
+
 }
 
 
