bb150be6bb84e999b938eddc32991ce54202e2b8
todreami <140941478+todreami@users.noreply.github.com>
2024-04-12 09:19:52 +0900

    64
    64

--- CodeKatatask/CodeKatatask/Program.cs
+++ CodeKatatask/CodeKatatask/Program.cs
@@ -1097,6 +1097,46 @@ public class Program
         return answer;
     }
 
+    //64번 체육복
+    public int solutionsixtyfour(int n, int[] lost, int[] reserve)
+    {
+        Array.Sort(lost);
+        Array.Sort(reserve);
+
+        foreach (int l in lost)
+        {
+
+            foreach (int r in reserve)
+            {
+                if (l == r)
+                {
+                    lost = lost.Where(x => x != l).ToArray();
+                    reserve = reserve.Where(x => x != r).ToArray();
+                    break;
+                }
+            }
+        }
+
+        int count = 0;
+        foreach (int l in lost)
+        {
+            for (int i = 0; i < reserve.Length; i++)
+            {
+
+                if (l - 1 == reserve[i] && reserve[i] != 0
+                     || l + 1 == reserve[i] && reserve[i] != 0)
+                {
+                    reserve[i] = 0;
+                    count++;
+                    break;
+                }
+            }
+        }
+        return n - lost.Length + count;
+    }
+
+
+
 
 }
 
