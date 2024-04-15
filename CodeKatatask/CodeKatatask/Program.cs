5c67723d88978f91fdbbd063e97fa179a5ff3dc9
todreami <140941478+todreami@users.noreply.github.com>
2024-04-15 09:22:48 +0900

    65
    65

--- CodeKatatask/CodeKatatask/Program.cs
+++ CodeKatatask/CodeKatatask/Program.cs
@@ -1135,8 +1135,46 @@ public class Program
         return n - lost.Length + count;
     }
 
+    //65번 문자열 나누기
+    public int solutionsixtyfive(string s)
+    {
+        int answer = 0;
+        int sameIdx = 0;
+        int diffIdx = 0;
+        char word = ' ';
+        bool start = true;
 
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (start == true)
+            {
+                word = s[i];
+                sameIdx++;
+                start = false;
+            }
+            else
+            {
+                if (s[i] == word)
+                {
+                    sameIdx++;
+                }
+                else
+                {
+                    diffIdx++;
+                }
+            }
 
+            if (sameIdx == diffIdx)
+            {
+                answer++;
+                sameIdx = 0;
+                diffIdx = 0;
+                start = true;
+            }
+            if (i == s.Length - 1) { if (start == false) { answer++; } }
+        }
+        return answer;
+    }
 
 }
 
