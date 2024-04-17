adb2627351b659c57f23ee943ea54c87a8764279
todreami <140941478+todreami@users.noreply.github.com>
2024-04-17 09:39:26 +0900

    67
    67

--- CodeKatatask/CodeKatatask/Program.cs
+++ CodeKatatask/CodeKatatask/Program.cs
@@ -1220,6 +1220,26 @@ public class Program
         return min;
     }
 
+    //67번 둘만의 암호
+    public string solutionsixtyseven(string s, string skip, int index)
+    {
+        string answer = "";
+        char temp;
+        for (int i = 0; i < s.Length; i++)
+        {
+            temp = s[i];
+            for (int j = 0; j < index;)
+            {
+                temp = (char)((int)temp + 1);
+                if (temp > 'z') { temp = 'a'; }
+                if (skip.Contains(temp) == true) { continue; }
+                j++;
+            }
+            answer += temp;
+        }
+        return answer;
+    }
+
 
 
 }
