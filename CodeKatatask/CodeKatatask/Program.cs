da5f7d0f0bea70c0db2f5218cf1eb4a096a203bc
todreami <140941478+todreami@users.noreply.github.com>
2024-04-19 09:24:05 +0900

    69
    69

--- CodeKatatask/CodeKatatask/Program.cs
+++ CodeKatatask/CodeKatatask/Program.cs
@@ -1261,6 +1261,55 @@ public class Program
         }
         return answer;
     }
+
+    //69번 성격 유형 검사하기
+    public string solutionsolutionsixtynine(string[] survey, int[] choices)
+    {
+        string answer = "";
+        string[,] keys = new string[4, 2]
+        {
+            {"R", "T" }, {"C", "F" }, {"J","M"}, {"A","N"}
+        };
+        int[,] values = new int[4, 2];
+
+        for (int i = 0; i < survey.Length; i++)
+        {
+            string sSurvey = "";
+            int choice = 0;
+
+            if (choices[i] > 4)
+            {
+                sSurvey = survey[i][1].ToString();
+                choice = (-1) * (4 - choices[i]);
+            }
+            else
+            {
+                sSurvey = survey[i][0].ToString();
+                choice = 4 - choices[i];
+            }
+
+            for (int j = 0; j < 4; j++)
+            {
+                for (int k = 0; k < 2; k++)
+                {
+                    if (keys[j, k] == sSurvey)
+                    {
+                        values[j, k] += choice;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < 4; i++)
+        {
+            if (values[i, 0] >= values[i, 1])
+                answer += keys[i, 0];
+            else if (values[i, 0] < values[i, 1])
+                answer += keys[i, 1];
+        }
+        return answer;
+    }
+
 }
 
 
