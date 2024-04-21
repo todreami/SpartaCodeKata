19c9bca5e974a775d86a5fb4d7e7c962498a784c
todreami <140941478+todreami@users.noreply.github.com>
2024-04-21 23:03:48 +0900

    70
    70

--- CodeKatatask/CodeKatatask/Program.cs
+++ CodeKatatask/CodeKatatask/Program.cs
@@ -1310,6 +1310,30 @@ public class Program
         return answer;
     }
 
+    //70번 바탕화면 정리
+    public int[] solutionnseventy(string[] wallpaper)
+    {
+        int[] answer = new int[] { int.MaxValue, int.MaxValue, 0, 0 };
+
+        for (int i = 0; i < wallpaper.Length; i++)
+        {
+            for (int j = 0; j < wallpaper[i].Length; j++)
+            {
+                if (wallpaper[i][j] == '#')
+                {
+                    answer[0] = Math.Min(answer[0], i);
+                    answer[1] = Math.Min(answer[1], j);
+                    answer[2] = Math.Max(answer[2], i + 1);
+                    answer[3] = Math.Max(answer[3], j + 1);
+                }
+            }
+        }
+        return answer;
+    }
+
+
+
+
 }
 
 
